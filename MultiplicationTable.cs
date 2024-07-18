05489c0eee5ea3841679fb6ae4172aa18871c235
Ivan Dyakov <dyakovivan@hotmail.com>
2024-07-18 19:01:56 +0200

    Добавьте файлы проекта.

---
+++ MultiplicationTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите размерность таблицы умножения:");
+            int tableSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write($"{"y/x",4} |");
+
+            for (int i = 1; i <= tableSize; i++)
+            {
+                Console.Write($"{i,4}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= tableSize + 1; i++)
+            {
+                Console.Write(" __ ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= tableSize; i++)
+            {
+                Console.Write($"{i,4} |");
+
+                for (int j = 1; j <= tableSize; j++)
+                {
+                    Console.Write($"{i * j,4}");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
